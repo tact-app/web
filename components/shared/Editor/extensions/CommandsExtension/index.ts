970b89c66e595fff527ad23754d71d0eb8a7af36
Kamil Samigullin <kamil@samigullin.info>
2023-01-21 21:09:51 +0300

    replace deprecated tiptap defaultOptions by addOptions

--- components/shared/Editor/extensions/CommandsExtension/index.ts
+++ components/shared/Editor/extensions/CommandsExtension/index.ts
@@ -4,24 +4,26 @@ import { Suggestion } from '@tiptap/suggestion';
 export const Commands = Extension.create({
   name: 'mention',
 
-  defaultOptions: {
-    suggestion: {
-      char: '/',
-      startOfLine: false,
-      command: ({
-        editor,
-        range,
-        props,
-      }: {
-        editor: Editor;
-        range: Range;
-        props: {
-          command: (chain: ChainedCommands, editor: Editor) => ChainedCommands;
-        };
-      }) => {
-        props.command(editor.chain().focus().deleteRange(range), editor).run();
+  addOptions() {
+    return {
+      suggestion: {
+        char: '/',
+        startOfLine: false,
+        command: ({
+          editor,
+          range,
+          props,
+        }: {
+          editor: Editor;
+          range: Range;
+          props: {
+            command: (chain: ChainedCommands, editor: Editor) => ChainedCommands;
+          };
+        }) => {
+          props.command(editor.chain().focus().deleteRange(range), editor).run();
+        },
       },
-    },
+    };
   },
 
   addProseMirrorPlugins() {
