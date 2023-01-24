168a2b188b360b45f4d0f60e6b969c768bcdc5f5
Dmitry Matveev <dmitryematveev@mail.ru>
2023-01-24 18:46:55 +0300

    Revert "tact-152 fixed overlay while drag-n-droping (#316)"
    This reverts commit 50361982abedbb6b501c015e254e39c2947c2677.

--- components/shared/ResizableGroup/ResizableGroupChild.tsx
+++ components/shared/ResizableGroup/ResizableGroupChild.tsx
@@ -32,7 +32,6 @@ export const ResizableGroupChild = observer(function ResizableGroupChild({
       position='relative'
       style={{
         width: store.widths.length ? store.getWidth(i) : config.width || 0,
-        clipPath: 'inset(0 0 0 0)'
       }}
       flexShrink={isFixed ? 0 : 1}
       onTransitionEnd={store.handleAnimationEnd}
