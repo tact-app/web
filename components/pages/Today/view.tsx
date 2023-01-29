0935288f08ad3e18acdbd1a1ae7e6308a4b1ead0
Dmitry Matveev <dmitryematveev@mail.ru>
2023-01-29 13:59:56 +0300

    tact-151: added onMinWidth function to resizableGroup store

--- components/pages/Today/view.tsx
+++ components/pages/Today/view.tsx
@@ -41,7 +41,7 @@ export const TodayView = observer(function TodayView() {
           >
             <FocusConfigurationBlock />
           </ResizableGroupChild>
-          <ResizableGroupChild index={1} config={store.resizableConfig[1]}>
+          <ResizableGroupChild index={1} config={{ ...store.resizableConfig[1], onMinWidth: store.openFullScreenCalendar }}>
             <ListsBlock />
           </ResizableGroupChild>
           <ResizableGroupChild
@@ -52,7 +52,7 @@ export const TodayView = observer(function TodayView() {
           >
             <TaskBlock />
           </ResizableGroupChild>
-          <ResizableGroupChild index={3} config={store.resizableConfig[3]}>
+          <ResizableGroupChild index={3} config={{ ...store.resizableConfig[3], onMinWidth: store.collapseCalendar }}>
             <CalendarBlock />
           </ResizableGroupChild>
         </ResizableGroup>
