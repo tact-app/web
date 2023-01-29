7894f26c138f494f099791d5431960d3710e5137
Alexander Petrushin <stuartrevol@gmail.com>
2023-01-29 22:27:53 +0300

    TACT-122: Added mailto support

--- helpers/validators.ts
+++ helpers/validators.ts
@@ -7,6 +7,11 @@ function isValidUrl(url: string) {
     return urlPattern.test(url);
 }
 
+function isValidMailTo(value: string) {
+    return new RegExp('mailto:([^?]+)', 'gi').test(value);
+}
+
 export const Validators = {
     isValidUrl,
+    isValidMailTo,
 };
