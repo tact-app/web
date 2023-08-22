70bdd2463ee7ca51908cdd893c99ff47479450b2
Kamil Samigullin <kamil@samigullin.info>
2023-08-22 17:16:12 +0300

    fix #1040: dev: sentry usage overflow

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,5 +5,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN,
   environment: process.env.APP_ENV || 'development',
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.0,
 });
